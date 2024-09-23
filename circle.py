44742c5a4c16ef4f4c804700f109ae9d47f7a322
Oleg <Oleg@HOME-PC>
2024-09-23 20:39:24 +0300

    docs: all functions documented

--- circle.py
+++ circle.py
@@ -2,9 +2,23 @@ import math
 
 
 def area(r):
+    ''' Возвращает площадь круга 
+            Параметры:
+                r (number): радиус круга
+            Возвращаемое значение:
+                area (number): площадь круга
+    '''
+
     return math.pi * r * r
 
 
 def perimeter(r):
+    ''' Возвращает периметр круга 
+            Параметры:
+                r (number): радиус круга
+            Возвращаемое значение:
+                perimeter (number): периметр
+    '''
+
     return 2 * math.pi * r
 
